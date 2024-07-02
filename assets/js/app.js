ce6400a084409da3127430983f2b98e692899dd0
Elany Ferreira <ellanysouza08@gmail.com>
2024-07-02 16:00:02 -0300

    search field

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,71 @@
 import config from "./chave.js";
 
+// https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${title}&language=en-US&page=1
+
+const ulCards = document.querySelector("[data-ulCard]");
+const inputFilme = document.querySelector('.inputFilme');
+const form = document.querySelector('.content_formulario');
+
+// Buscando todos os filmes
 async function getMovies() {
   const url = `https://api.themoviedb.org/3/movie/popular?api_key=${config.apiKey}&language=pt-BR`;
 
-  const fetchMovies = await fetch(url);
+  const filmes = await fetch(url);
 
-  const {results}= await fetchMovies.json();
+  const { results } = await filmes.json();
 
   return results;
 }
 
-const ulCards = document.querySelector("[data-ulCard]");
+window.onload = async function () {
+  const movies = await getMovies();
 
-function criarCardFilmes(movie) {
+  movies.forEach((movie) => 
+    ulCards.appendChild(criarElementoLi(movie))
+  )
+}
 
-  const {title, poster_path, vote_average, release_date, overview} = movie;
+// Busca filmes por id
+async function getByFilmes(title) {
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${config.apiKey}&query=${title}&language=en-US&page=1`;
+  const filmes = await fetch(url);
+
+  const {results} = await filmes.json();
+
+  return results;
+}
+
+async function searchFilmes(){
+  if (inputFilme.value != "") {
+    ulCards.innerHTML = "";
+    const movies = await getByFilmes(inputFilme.value);
+
+    // console.log(movies);
+
+    movies.forEach((movie) => {
+      ulCards.appendChild(criarElementoLi(movie));
+      console.log(movie);
+    })
+  } 
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  searchFilmes();
+})
+
+// Cria elemento li
+function criarElementoLi(movie) {
+  const { title, poster_path, vote_average, release_date, overview } = movie;
   const isFavorited = false;
 
   const dataFilme = new Date(release_date).getFullYear();
-
-  const image = `https://image.tmdb.org/t/p/w500${poster_path}`
+  const image = `https://image.tmdb.org/t/p/w500${poster_path}`;
 
   const li = document.createElement("li");
+  li.classList.add("content");
+
   let imgUrl;
   let favorito;
 
@@ -34,36 +78,29 @@ function criarCardFilmes(movie) {
   }
 
   li.innerHTML = `
-        <div class="content">
-            <img src="${image}" alt="" />
+    <div class="content">
+     <img src="${image}" alt="" />
 
-            <div class="content_info">
-              <p>${title} - ${dataFilme}</p>
-              <div class="movie_info">
-                <div class="movie_detals">
-                  <img src="assets/img/Star.svg" alt="" />
-                  <p>${vote_average}</p>
-                </div>
+    <div class="content_info">
+      <p>${title} - ${dataFilme}</p>
+        <div class="movie_info">
+          <div class="movie_detals">
+            <img src="assets/img/Star.svg" alt="" />
+            <p>${vote_average}</p>
+          </div>
 
-                <div class="movie_detals">
-                  <img src="${imgUrl}" alt="" />
-                  <p>${favorito}</p>
-                </div>
-              </div>
-            </div>
+          <div class="movie_detals">
+            <img src="${imgUrl}" alt="" />
+            <p>${favorito}</p>
+          </div>
         </div>
+      </div>
+    </div>
 
-        <p class="description">
-            ${overview}
-        </p>
-    `;
+    <p class="description">
+      ${overview}
+    </p>
+  `;
 
-  return li;
+  return li
 }
-
-window.onload = async function () {
-  const movies = await getMovies();
-  movies.forEach((movie) => {
-    ulCards.appendChild(criarCardFilmes(movie));
-  });
-};
